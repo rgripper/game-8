a5c7b7415adac05dab4befe2eb6355d456544e0d
Vladimir <azuresplash@gmail.com>
2020-08-16 22:04:54 +0200

    fixed more yarn monorepo issues

--- packages/net/dist/client.js
+++ packages/net/dist/client.js
@@ -12,10 +12,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToServer = void 0;
 const rxjs_1 = require("rxjs");
 const control_commands_1 = require("./control-commands");
-const operators_1 = require("rxjs/dist/types/operators");
+const operators_1 = require("rxjs/operators");
 function connectToServer(socket, authToken) {
     return __awaiter(this, void 0, void 0, function* () {
-        return yield rxjs_1.firstValueFrom((socket.isOpen() ? rxjs_1.from([true]) : rxjs_1.fromEvent(socket, 'open')).pipe(operators_1.timeoutWith(1000, rxjs_1.throwError(new Error('Timed out waiting for socket to open'))), operators_1.tap(() => socket.send(control_commands_1.AuthorizationPrefix + authToken)), operators_1.mergeMap(() => rxjs_1.fromEvent(socket, 'message').pipe(operators_1.first(), operators_1.timeoutWith(1000, rxjs_1.throwError(new Error('Timed out waiting for an authorization message'))), operators_1.tap(message => {
+        return yield rxjs_1.firstValueFrom((socket.isOpen() ? rxjs_1.from([true]) : rxjs_1.fromEvent(socket, 'open')).pipe(operators_1.timeout({ each: 1000, with: () => rxjs_1.throwError(new Error('Timed out waiting for socket to open')) }), operators_1.tap(() => socket.send(control_commands_1.AuthorizationPrefix + authToken)), operators_1.mergeMap(() => rxjs_1.fromEvent(socket, 'message').pipe(operators_1.first(), operators_1.timeout({
+            each: 1000,
+            with: () => rxjs_1.throwError(new Error('Timed out waiting for an authorization message')),
+        }), operators_1.tap(message => {
             if (message.data !== control_commands_1.AuthorizationSuccessful) {
                 throw new Error('Expected message data to by AuthorizationSuccessful but was something else');
             }
