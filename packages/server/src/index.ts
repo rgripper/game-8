ffda321e41ab1cfdea1dcd31f2c0b46349d7e128
Vladimir <azuresplash@gmail.com>
2020-09-19 22:13:19 +0200

    Added back startServer

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,10 @@
-import { Subject } from 'rxjs';
-import { Diff, SimCommand, WorldParams } from './sim/sim';
+import { merge, Observable, Subject } from 'rxjs';
+import { bufferTime, last, switchMap, tap } from 'rxjs/operators';
+import { BehaviourType, Diff, ModelType, SimCommand, WorldParams } from './sim/sim';
 import minimist from 'minimist';
 import http from 'http';
+import WebSocket from 'ws';
+import { createSimpleServer, waitForClients } from 'sim-net';
 
 type OwnedCommandWrapper = {
     command: SimCommand;
@@ -34,6 +37,89 @@ async function main() {
 
     // TODO: terminate with subscription.unsubscribe()
     // process.exit();
+}
+
+function createSeverObservable({ port }: { port: number }) {
+    return new Observable<WebSocket.Server>(subscriber => {
+        const server = new WebSocket.Server({ port });
+        subscriber.next(server);
+        return () => {
+            server.close();
+        };
+    });
+}
+
+function startServer(playersCount: number) {
+    return createSeverObservable({ port: serverPort }).pipe(
+        switchMap(server =>
+            waitForClients({
+                server,
+                getClientIdByToken: x => x,
+                expectedClientCount: playersCount,
+                authTimeout: 200,
+            }),
+        ),
+        switchMap(async clients => {
+            console.log('Clients were received', clients);
+            const simpleServer = createSimpleServer<SimCommand, Diff[]>(clients);
+            const sim = await createSimInRust({ size: { height: 500, width: 500 } });
+            return {
+                simpleServer,
+                sim,
+            };
+        }),
+        switchMap(({ simpleServer, sim }) => {
+            const initCommands = [
+                { type: 'AddPlayer', player: { id: 1 } },
+                { type: 'AddPlayer', player: { id: 2 } },
+                {
+                    type: 'AddEntity',
+                    entity_params: {
+                        model_type: ModelType.Human,
+                        behaviour_type: BehaviourType.Actor,
+                        boundaries: { top_left: { x: 10, y: 10 }, size: { height: 32, width: 32 } },
+                        health: { max: 100, current: 100 },
+                        player_id: 1,
+                        rotation: 0,
+                    },
+                },
+                {
+                    type: 'AddEntity',
+                    entity_params: {
+                        model_type: ModelType.Human,
+                        behaviour_type: BehaviourType.Actor,
+                        boundaries: { top_left: { x: 200, y: 200 }, size: { height: 32, width: 32 } },
+                        health: { max: 100, current: 100 },
+                        player_id: 2,
+                        rotation: 0,
+                    },
+                },
+            ];
+
+            const initDiffs = sim(initCommands.map(command => ({ command, player_id: null } as OwnedCommandWrapper)));
+            console.log('Sending init diffs', initDiffs);
+            simpleServer.sendFrame(initDiffs);
+
+            const finalFrame = simpleServer.commands.pipe(
+                bufferTime<{ command: SimCommand; socketId: string }>(10),
+                tap(wrappedCommands => {
+                    const diffs = sim(
+                        wrappedCommands.map<OwnedCommandWrapper>(sc => ({
+                            command: sc.command,
+                            player_id: parseInt(sc.socketId),
+                        })),
+                    );
+                    if (diffs.length > 0) {
+                        console.log('Sending update diffs', diffs);
+                        simpleServer.sendFrame(diffs);
+                    }
+                }),
+                last(),
+            );
+
+            return finalFrame;
+        }),
+    );
 }
 
 async function startHttpServer(playersCount: number, restartSubject: Subject<undefined>) {
